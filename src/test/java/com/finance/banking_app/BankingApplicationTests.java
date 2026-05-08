76037f732a70f24a05006262f2d519b00bf50c6a
venkatapadma <padmaganapathi568@gmail.com>
2026-05-08 11:08:41 +0100

    created application-test.properties

--- src/test/java/com/finance/banking_app/BankingApplicationTests.java
+++ src/test/java/com/finance/banking_app/BankingApplicationTests.java
@@ -2,8 +2,10 @@ package com.finance.banking_app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BankingApplicationTests {
 
     @Test
